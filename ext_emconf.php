6cdefa0dfd343bced1cccf4d0fba1add04969ada
Chediard <christophe.hediard@outlook.fr>
2016-06-03 10:53:56 +0200

    Add files via upload

---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pradinestemplate".
+ *
+ * Auto generated 23-08-2014 13:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'test',
+    'description' => 'ext test',
+    'category' => 'fe',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.2 - 8.99.99',
+			
+        ),
+        'conflicts' => array(),
+    ),
+    'state' => 'stable',
+    'uploadfolder' => 1,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Hediard',
+    'author_email' => 'christophe.hediard@outlook.fr',
+    'author_company' => 'CDG47',
+    'version' => '7.6.1',
+);
+?>
